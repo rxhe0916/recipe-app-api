2a4644c0848220059fc891afcac524c50e91054f
rxhe0916 <rxhe0916@gmail.com>
2020-02-06 18:29:42 -0500

    Added create ingredient feature.

--- app/user/tests/test_ingredients_api.py
+++ app/user/tests/test_ingredients_api.py
@@ -31,7 +31,7 @@ class PrivateIngredientsAPITests(TestCase):
     def setUp(self):
         self.client = APIClient()
         self.user = get_user_model().objects.create_user(
-            'test@londonappdev.com',
+            'test@rxhe0916.com',
             'testpass'
         )
         self.client.force_authenticate(self.user)
@@ -51,7 +51,7 @@ class PrivateIngredientsAPITests(TestCase):
     def test_ingredients_limited_to_user(self):
         """Test that only ingredients for authenticated user are returned"""
         user2 = get_user_model().objects.create_user(
-            'other@londonappdev.com',
+            'other@rxhe0916.com',
             'testpass'
         )
         Ingredient.objects.create(user=user2, name='Vinegar')
@@ -63,3 +63,21 @@ class PrivateIngredientsAPITests(TestCase):
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(len(res.data), 1)
         self.assertEqual(res.data[0]['name'], ingredient.name)
+
+    def test_create_ingredient_successful(self):
+        """Test creating a new ingredient"""
+        payload = {'name': 'Cabbage'}
+        self.client.post(INGREDIENTS_URL, payload)
+
+        exists = Ingredient.objects.filter(
+            user=self.user,
+            name=payload['name']
+        ).exists()
+        self.assertTrue(exists)
+
+    def test_create_ingredient_invalid(self):
+        """Test creating invalid ingredient fails"""
+        payload = {'name': ''}
+        res = self.client.post(INGREDIENTS_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
